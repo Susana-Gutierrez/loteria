2b8a1abf5ed4f6cccb43851bcea1ba889d962bd6
Susana Gutierrez <susanagabriela_ap@hotmail.com>
2022-05-08 20:43:40 -0700

    Complted changes for testing.

--- client/lib/app-connection.js
+++ client/lib/app-connection.js
@@ -1,6 +1,6 @@
 
 const io = require('socket.io-client');
-const socket = io('http://localhost:3001');
+const socket = io(`/:${process.env.PORT}` || 3001);
 
 function AppConnection(action) {
 
--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: `/:${process.env.PORT}` || 3000,
     methods: ['GET', 'POST']
   }
 });
